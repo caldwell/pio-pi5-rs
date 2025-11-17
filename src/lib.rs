f4a0932de94db9bc0ca9b0d4a78e9a39ebc9bc34
David Caldwell <david@porkrind.org>
2025-11-16 21:28:26 -0800

    lib.rs: impl std::error::Error for Error

--- src/lib.rs
+++ src/lib.rs
@@ -106,6 +106,9 @@ pub enum Error {
     BadGPIO { gpio: u16, max: usize },
 }
 
+impl std::error::Error for Error {
+}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
