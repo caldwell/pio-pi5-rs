c66d2e0d6f40c78588afb3203b40541bf74517ce
David Caldwell <david@porkrind.org>
2025-11-18 14:29:43 -0800

    move gen scripts into gen/

--- gen/gen-proc-pio.sh
+++ gen/gen-proc-pio.sh
@@ -2,5 +2,5 @@
 
 set -x
 curl -s -L https://github.com/raspberrypi/utils/raw/refs/heads/master/piolib/include/hardware/regs/proc_pio.h \
-    | ./gen-proc-pio.rb > src/proc-pio.rs.new
+    | "$(dirname $0)"/gen-proc-pio.rb > src/proc-pio.rs.new
 mv src/proc-pio.rs{.new,}
